612fdfabf1a1027c2679119459a51b8b711173e7
b-612 <fromtheorion@yandex.ru>
2020-01-30 18:18:48 +0300

    Убирает баг с лишним watch

--- gulpfile.js
+++ gulpfile.js
@@ -109,7 +109,6 @@ gulp.task('server', function () {
 
     gulp.watch('source/sass/**/*.{scss,sass}', gulp.series('cssclean', 'css'));
     gulp.watch('source/*.html', gulp.series('copyhtml')).on('change', server.reload);
-    gulp.watch('source/img/svgsprite', gulp.series('svgsprite', 'refresh'));
     gulp.watch('source/js/*.js', gulp.series('jsmin', 'refresh'));
 });
 
